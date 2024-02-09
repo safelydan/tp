b5f71021c327d2a187e5f9c48559700444c6c417
daniel araujo <daniellealuau@gmail.com>
2024-02-08 23:06:13 -0300

    primeira aula

---
+++ jsPuro.js
@@ -0,0 +1,11 @@
+const soma = (a, b) =>{
+    return a + b;
+}
+
+// exemplo dos propblemas do javascript
+console.log(soma(10, 20));
+console.log(soma('129', 2));
+console.log(soma(true, true))
+console.log(soma(false, false))
+console.log(soma(false, true))
+console.log(soma(1.5, ' true'))
